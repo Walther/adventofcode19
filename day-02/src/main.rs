632842bab4383c9ab126de09c44baffc93bd7b8c
Walther <veeti.haapsamo@gmail.com>
2019-12-02 13:57:16 +0200

    Refactor: compute takes mut vec instead of ref

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -17,22 +17,21 @@ fn part1(input: &Vec<usize>) {
     // "Before running the program, replace position 1 with the value 12 and replace position 2 with the value 2."
     memory[1] = 12;
     memory[2] = 2;
-    let result = compute(&memory);
+    let result = compute(memory);
     println!("Part 1: {}", result);
 }
 
 fn part2(input: &Vec<usize>) {
-    let orig_memory = input.clone();
     // Find pair of inputs that give target result
     let mut result_noun = 0;
     let mut result_verb = 0;
     let target_result = 19690720;
     'outer_loop: for noun in 0..=99 {
         for verb in 0..=99 {
-            let mut memory = orig_memory.clone();
+            let mut memory = input.clone();
             memory[1] = noun;
             memory[2] = verb;
-            let result = compute(&memory);
+            let result = compute(memory);
             if result == target_result {
                 result_noun = noun;
                 result_verb = verb;
--- intcode/src/lib.rs
+++ intcode/src/lib.rs
@@ -1,8 +1,7 @@
 /// Implements the Intcode computer as specified by Advent of Code 2019.
 /// Given an input Vec of integers, runs the computations and returns the result
-/// integer.
-pub fn compute(input: &Vec<usize>) -> usize {
-    let mut memory = input.clone();
+/// integer. Mutates memory during computation.
+pub fn compute(mut memory: Vec<usize>) -> usize {
     let mut instruction_pointer = 0;
 
     loop {
@@ -48,28 +47,28 @@ mod tests {
     #[test]
     fn day2_test1() {
         let input = vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
-        let result = crate::compute(&input);
+        let result = crate::compute(input);
         assert_eq!(result, 3500);
     }
 
     #[test]
     fn day2_test2() {
         let input = vec![1, 0, 0, 0, 99];
-        let result = crate::compute(&input);
+        let result = crate::compute(input);
         assert_eq!(result, 2);
     }
 
     #[test]
     fn day2_test3() {
         let input = vec![2, 3, 0, 3, 99];
-        let result = crate::compute(&input);
+        let result = crate::compute(input);
         assert_eq!(result, 2);
     }
 
     #[test]
     fn day2_test4() {
         let input = vec![1, 1, 1, 4, 99, 5, 6, 0, 99];
-        let result = crate::compute(&input);
+        let result = crate::compute(input);
         assert_eq!(result, 30);
     }
 }
